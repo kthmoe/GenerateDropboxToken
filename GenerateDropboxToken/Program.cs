292bf0f752297e204b78790d41bec10249594e89
Khin Thidar Moe <Khin Thidar Moe@DESKTOP-CEVPNU7>
2022-08-08 08:47:16 +0630

    by ktdm

--- GenerateDropboxToken/Program.cs
+++ GenerateDropboxToken/Program.cs
@@ -28,27 +28,32 @@ namespace GenerateDropboxToken
             ChromeOptions option = new ChromeOptions();
             var service = ChromeDriverService.CreateDefaultService(AppDomain.CurrentDomain.BaseDirectory);
             IWebDriver chrome = new ChromeDriver(service, option, TimeSpan.FromMinutes(10));
-
+            //EdgeOptions option = new EdgeOptions();
+            //var service = EdgeDriverService.CreateDefaultService(AppDomain.CurrentDomain.BaseDirectory);
+            //IWebDriver chrome = new EdgeDriver(service, option, TimeSpan.FromMinutes(10));
+            Thread.Sleep(10000);
             chrome.Url = url;
-
+            Thread.Sleep(10000);
             string username = "office@capital-knowledge.co.jp"; string password = "capitaloo13";
-            chrome.Url = url;
-
-            Thread.Sleep(3000);
+           
+            Thread.Sleep(10000);
             //// method Keys.chord            
             chrome.FindElement(By.Name("login_email")).SendKeys(username);
+            Thread.Sleep(10000);
             chrome.FindElement(By.Name("login_password")).SendKeys(password);
 
-            Thread.Sleep(10000);
+            Thread.Sleep(100000);
             chrome.FindElement(By.XPath("//button[@type='submit']")).Click();
             Thread.Sleep(200000);
             chrome.FindElement(By.Id("generate-token-button")).Click();
-            Thread.Sleep(150000);
+            Thread.Sleep(20000);
             WebElement l = (WebElement)chrome.FindElement(By.Id("generated-token"));
             // enter texts
             l.SendKeys("Selenium");
             // get value attribute with getAttribute()
             String val = l.GetAttribute("value");
+            String path = @"C:\Backup\Dbpath.ini";
+            System.IO.File.WriteAllText(path, System.IO.File.ReadAllText(path).Replace("TokenKey=","["+ val+"]"));
             Console.Write("Entered text is: " + val);
 
 
